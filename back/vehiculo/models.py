2824943a3d8dea8cb53d720fcb47f4e025013738
Margarita-Bravo <embravo3329@gmail.com>
2025-10-02 16:33:00 -0300

    corrigiendo categoria id

--- back/vehiculo/models.py
+++ back/vehiculo/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from user.models import Cliente
 # from user.models import UserAccount
 from datetime import datetime
+from categoria.models import Categoria
 
 # Create your models here.
 
@@ -9,7 +10,14 @@ from datetime import datetime
 class Vehiculo(models.Model):
     marca = models.CharField(max_length=100, default="Desconocido")  # Nueva columna ejemplo Honda, Yamaha
     modelo = models.CharField(max_length=100)  #ejemplo Civic, YZF-R3
-    categoria = models.CharField(max_length=100, default="Desconocido")  # Nueva columna (Ej: Moto, Auto, Camión)
+    # categoria = models.CharField(max_length=100, default="Desconocido")  # Nueva columna (Ej: Moto, Auto, Camión)
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="vehiculos"
+    )
     tipo = models.CharField(max_length=100, default="Otro") # ejemplo autos(sedán, pickup) ejemplo moto (deportiva, scooter, enduro)
     anio_fabricacion = models.PositiveIntegerField(default=2000)  # Nueva columna
     # imagen = models.ImageField(upload_to='photos/%Y/%m/')
