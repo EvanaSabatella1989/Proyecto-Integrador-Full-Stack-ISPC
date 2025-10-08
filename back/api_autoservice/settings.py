6bfe2974953d9be3eef4c97858ac055c8cc116b8
Margarita-Bravo <embravo3329@gmail.com>
2025-10-08 13:02:53 -0300

    borrando comentarios

--- back/api_autoservice/settings.py
+++ back/api_autoservice/settings.py
@@ -9,7 +9,7 @@ from decouple import config
 
 
 
-# --- CONFIGURACIÓN GLOBAL DE EMAIL (válida para todo el proyecto) ---
+# --- configuración para que los mensajes lleguen al correo del administrador ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -21,16 +21,6 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 ADMIN_EMAIL = config('ADMIN_EMAIL', default=EMAIL_HOST_USER)
 
 
-
-
-# configuracion para el envio de correos a  Mailtrap y hacer las pruebas
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.mailtrap.io'  # Servidor SMTP de Mailtrap
-# EMAIL_PORT = 2525  # Puerto de Mailtrap (Usa 587 si necesitas TLS)
-# EMAIL_HOST_USER = 'c1e209a5685478'  # Copia el usuario desde Mailtrap
-# EMAIL_HOST_PASSWORD = '458a83918d03ff'  # Copia la contraseña desde Mailtrap
-# EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = False  # No usar SSL  
 
 
 
