2aec895f274f005281566f13170074b5e9a71f52
Margarita Bravo <embravo3329@gmail.com>
2025-10-29 12:36:08 -0300

    modificando estilo de login y registro

--- Angular/src/app/pages/carrito/carrito.component.ts
+++ Angular/src/app/pages/carrito/carrito.component.ts
@@ -82,8 +82,8 @@ export class CarritoComponent implements OnInit {
         console.log("Respuesta de Mercado Pago:", resp);
 
         if (resp.init_point) {
-          window.open(resp.init_point, '_blank');  // ✅ Abre en nueva pestaña
-          // window.location.replace(resp.init_point)
+          // window.open(resp.init_point, '_blank');  // ✅ Abre en nueva pestaña
+           window.location.replace(resp.init_point)
           this.status = 'success';
          
         } else {
--- back/venta/admin.py
+++ back/venta/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Venta
 
 # Register your models here.
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ('numero_factura', 'cliente', 'tipo_pago', 'estado', 'total', 'fecha_pago')
+    list_filter = ('estado', 'tipo_pago', 'fecha_pago')
+    search_fields = ('numero_factura', 'cliente__user__email', 'cliente__user__first_name', 'cliente__user__last_name')
+    date_hierarchy = 'fecha_pago'
+    
+    readonly_fields = ('numero_factura', 'fecha_pago')
+
+    fieldsets = (
+        ('Información de la venta', {
+            'fields': ('numero_factura', 'cliente', 'total', 'tipo_pago', 'estado', 'fecha_pago')
+        }),
+    )
+    
+
+admin.site.register(Venta, VentaAdmin)
